d719d8be922e2c84fb65e1abbe3f92dfb094fd02
易镇艺 <yizhenyi@fangdd.com>
2020-07-15 17:47:53 +0800

    修改test.js为testd.js避免测试

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const { resolve } = require('path');
 const { js2dtFromFile } = require('./lib');
 const config = require('./lib/config');
 
-const path = resolve(__dirname, './test.js');
+const path = resolve(__dirname, './testd.js');
 config.overwrite = true;
 js2dtFromFile(path);

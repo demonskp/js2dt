d39dae52674ddedec44c28831eac191ba1403b94
易镇艺 <yizhenyi@fangdd.com>
2020-07-15 11:48:17 +0800

    类测试编写

---
+++ test/traverse/classDeclaration.test.js
@@ -0,0 +1,15 @@
+describe('类转换测试', () => {
+  test('基本类测试', () => {
+    const classStr = `
+    class test {
+      name='dave';
+
+      age=100;
+
+      say=(a, b) => a + b;
+
+      jkl;
+    }
+    `;
+  });
+});

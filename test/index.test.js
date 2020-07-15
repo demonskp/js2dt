571b3d31c5c30bfc5bf09ee14a072c328d5205de
易镇艺 <yizhenyi@fangdd.com>
2020-07-15 17:42:15 +0800

    增加基础jest测试

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,73 @@
+const { transformCode2Ast } = require('../lib/utils');
+const { getDtsString } = require('../lib');
+
 test('测试', () => {
-  const result = 'Hello sam';
-  expect(result).toBe('Hello sam');
+  const result = `const path = require('path');
+  const fs = require('fs');
+  const { tsObjectPattern } = require('../temp/objectTemp');
+  const { getTsFunctionTypeAnnotation } = require('./functionDeclaration');
+  const config = require('../config');
+
+  // =====================
+
+  const a = 1;
+  const b = '2';
+  const c = function test(a, b) {
+    return a + b;
+  };
+  const d = (a, b) => a + b;
+  const e = {
+    a: 1,
+    b: '',
+  };
+  const name = null;
+
+  //= ===========
+
+  class test extends father {
+    a=1;
+
+    b='2';
+
+    c=function (a, b) {
+      return a + b;
+    };
+
+    d=(a, b) => a + b;
+
+    e={
+      a: 1,
+      b: '',
+    };
+  }
+
+  //= ======
+
+  function funcTest({ a, b }, ...rest) {
+    return '测试';
+  }
+
+  //= =========
+
+  funcTest.a = 1;
+  funcTest.b = '2';
+  funcTest.c = function test(a, b) {
+    return a + b;
+  };
+  funcTest.d = (a, b) => a + b;
+  funcTest.e = {
+    a: 1,
+    b: '',
+  };
+  funcTest.name = null;
+
+  //= =============
+
+  module.exports = {
+    funcTest, a, b,
+  };
+  `;
+
+  const ast = transformCode2Ast(result);
+  expect(getDtsString(ast)).toMatch('declare namespace funcTest {');
 });
